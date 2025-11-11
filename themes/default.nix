a60907491a25bf7c380bf7abd325bd6b38dd0ed6
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-11-11 23:26:15 +0100

    refactor(themes): install all themes as part of root derivation

--- themes/default.nix
+++ themes/default.nix
@@ -12,9 +12,28 @@ let
 
     directory = ./sets;
   };
+
+  version = "1.0.0";
 in
-runCommand "nixkraken-themes" {
-  passthru = allThemes // {
-    all = lib.attrValues allThemes;
-  };
-} "touch $out"
+runCommand "nixkraken-themes"
+  {
+    inherit version;
+
+    passthru = allThemes;
+  }
+  ''
+    mkdir -p $out
+    pushd $out
+    for theme in ${
+      lib.concatStringsSep " " (
+        lib.mapAttrsToList (theme: drv: "${theme}:${drv.version}:${drv.out}") allThemes
+      )
+    }; do
+      name=$(echo $theme | cut -d: -f1)
+      version=$(echo $theme | cut -d: -f2)
+      path=$(echo $theme | cut -d: -f3)
+      mkdir $name
+      cp $path/* $name
+      echo $version > $name/VERSION
+    done
+  ''

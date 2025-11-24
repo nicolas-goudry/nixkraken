75633cb22004f0655001b24fd7d337b4b22344ff
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-11-24 01:24:11 +0100

    chore: update renamed options

--- modules/user/profile-options.nix
+++ modules/user/profile-options.nix
@@ -3,8 +3,11 @@
 {
   email = lib.mkOption {
     type = with lib.types; nullOr str;
-    default = config.programs.git.userEmail;
-    defaultText = "config.programs.git.userEmail";
+    # TODO can be removed in the future when deprecated `config.programs.git.userEmail` is removed
+    default =
+      config.programs.git.userEmail
+        or (lib.attrByPath [ "settings" "user" "email" ] null config.programs.git);
+    defaultText = "config.programs.git.userEmail or config.programs.git.settings.user.email";
     example = "email@example.com";
     description = ''
       Email to use as commit author email.
@@ -20,8 +23,11 @@
 
   name = lib.mkOption {
     type = with lib.types; nullOr str;
-    default = config.programs.git.userName;
-    defaultText = "config.programs.git.userName";
+    # TODO can be removed in the future when deprecated `config.programs.git.userName` is removed
+    default =
+      config.programs.git.userName
+        or (lib.attrByPath [ "settings" "user" "name" ] null config.programs.git);
+    defaultText = "config.programs.git.userName or config.programs.git.settings.user.name";
     example = "John Doe";
     description = ''
       Name to use as commit author name.
